a08d8283c2c0632ba2ca9f3c310736089f7830d8
JasonWorger <jasonworger@gmail>
2020-07-26 20:27:23 +0000

    database layer and modified templates

--- flask-app/application/routes.py
+++ flask-app/application/routes.py
@@ -3,24 +3,14 @@ from flask import render_template
 # import the app object from the ./application/__init__.py
 from application import app
 # define routes for / & /home, this function will be called when these are accessed
+from application.models import Posts
 
-blogData = [
-    {  
-        "name": {"first":"John", "last":"Doe"},
-        "title":"First Post",
-        "content":"This is some blog data for Flask lectures"
-    },
-    {   
-        "name": {"first":"Jane", "last":"Doe"},
-        "title":"Second Post",
-        "content":"This is even more blog data for Flask lectures"
-    }
-]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home', posts=blogData)
+    postData = Posts.query.all()
+    return render_template('home.html', title='Home', posts=postData)
 
 @app.route('/about')
 def about():
@@ -30,4 +20,6 @@ def about():
 def login():
     return render_template('login.html', title='Login')
 
-
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
